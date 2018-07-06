1bcc13a3047e97642b2ca45a13a03e3fa7cfc3f8
juliehudspeth <juliejhudspeth@gmail.com>
2018-07-05 22:52:19 -0400

    initial commit
    initial commit

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './BusinessList.css';
+
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+         {
+                    this.props.businesses.map(function(business, i) {
+                        return <Business key={business.id} business={business} />;
+                    })
+        }
+      </div>
+    );
+  }
+}
+
+export default BusinessList;
